adbcc18bbbf3b46b11c50993dea5a5df995f88d0
PrajwalKN9071 <96880042+PrajwalKN9071@users.noreply.github.com>
2022-12-30 20:27:39 +0530

    Create runbook.ps1

---
+++ runbook.ps1
@@ -0,0 +1,24 @@
+param
+(
+    [Parameter (Mandatory= $true)]
+    [string]$AKSResourceGroup,
+    [Parameter (Mandatory= $true)]
+    [string]$AKSName,
+    [Parameter (Mandatory= $true)]
+    [string]$subscriptionID,
+    [Parameter (Mandatory= $true,HelpMessage="start or stop")]
+    [string]$Action
+)
+Connect-AzAccount -Identity
+Set-AzContext -SubscriptionId $subscriptionID
+
+if ($Action -eq "start") {
+    Get-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+    
+    Start-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+}
+elseif ($Action -eq "stop") {
+    Get-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+    
+    Stop-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+}
